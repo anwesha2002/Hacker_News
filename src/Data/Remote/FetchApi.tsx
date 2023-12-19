56f6fb427581a6168d9c94260328abf440aba724
anwesha <ANWESHA@DESKTOP-ALJFO4P.com>
2023-12-19 01:12:06 -0500

    refac: changing API to https from http

--- src/Data/Remote/FetchApi.tsx
+++ src/Data/Remote/FetchApi.tsx
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export function FetchDataFromApi(query : string, page : number){
     return(
-        axios.get(`http://hn.algolia.com/api/v1/search?query=${query}&page=${page}`)
+        axios.get(`https://hn.algolia.com/api/v1/search?query=${query}&page=${page}`)
     )
 }
 
 export function FetchDetail(id : string | undefined){
     return(
-        axios.get(`http://hn.algolia.com/api/v1/items/${id}`)
+        axios.get(`https://hn.algolia.com/api/v1/items/${id}`)
     )
 }
